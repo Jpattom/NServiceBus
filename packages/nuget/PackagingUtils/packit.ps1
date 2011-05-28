43e5268a4e81338b9b8457c60464d0a459ef32ee
jpattom <joseph.pattom@gmail.com>
2011-05-28 15:21:22 +0530

    Fixed the issues on fie=rst draft, ready for the package NServiceBus.Host --Pending: PAcking of Library and Executables to pack

--- packages/nuget/PackagingUtils/packit.ps1
+++ packages/nuget/PackagingUtils/packit.ps1
@@ -1,3 +1,19 @@
-import-module .\packit.psm1
-invoke-packit "NserviceBus.Host" 
-remove-module packit
+if(Test-Path ./RunOnce.ps1)
+{
+	./RunOnce.ps1
+	Write-Host "Please restart and execute me a again"
+	
+}
+$nugetAvailable = Get-Command Nuget 
+if($nugetAvailable -eq $null)
+{
+	Write-Host "Nuget Not Available......."
+	Write-Host "Please Copy the nuget.exe to any folder in [env]path and execute me again"
+	
+}
+else
+{
+	import-module ./packit.psm1
+	invoke-packit "NServiceBus.Host" "" @{NServiceBus="<version>"}
+	remove-module packit
+}
--- packages/nuget/PackagingUtils/packit.psm1
+++ packages/nuget/PackagingUtils/packit.psm1
@@ -11,17 +11,57 @@ $script:packit.package_projectUrl = "http://nservicebus.com/"
 $script:packit.package_requireLicenseAcceptance = $true;
 $script:packit.package_tags = "nservicebus servicebus msmq cqrs publish subscribe"
 $script:packit.package_version = "2.5"
+$script:packit.package_iconUrl = "http://images.nservicebus.com/nServiceBus_Logo.png"
 $script:packit.build_Location = "..\..\..\Build"
-$script:packit.versionAssembly = $script:packit.build_Location + "\nservicebus\NServiceBus.dll"
+$script:packit.versionAssemblyName = $script:packit.build_Location + "\nservicebus\NServiceBus.dll"
+$script:packit.packageOutPutDir = ".\packages"
 
 Export-ModuleMember -Variable "packit"
 
-function GetVersionFromVersionAssembly()
+$VesionPlaceHolder = "<version>"
+
+function PuchPackage($packageName)
 {
-	$versionAssemblyLocation = Resolve-Path -Path $script:packit.versionAssembly
-	$Myasm = [System.Reflection.Assembly]::Loadfile($versionAssemblyLocation)
-	return $Myasm.Version;
+$keyfile = ""
+$packagespath = resolve-path $script:packit.packageOutPutDir
+ 
+#if(-not (test-path $keyfile)) {
+#  throw "Could not find the NuGet access key at $keyfile. If you're not Jeremy, you shouldn't be running this script!"
+#}
+#else {
+  pushd $packagespath
+ 
+  # get our secret key.
+#  $key = get-content $keyfile
+ 
+  # Find all the packages and display them for confirmation
+  $packages = dir $packageName
+  write-host "Packages to upload:"
+  $packages | % { write-host $_.Name }
+ 
+  # Ensure we haven't run this by accident.
+  $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Uploads the packages."
+  $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Does not upload the packages."
+  $options = [System.Management.Automation.Host.ChoiceDescription[]]($no, $yes)
+ 
+  $result = $host.ui.PromptForChoice("Upload packages", "Do you want to upload the NuGet packages to the NuGet server?", $options, 0) 
+ 
+  # Cancelled
+  if($result -eq 0) {
+    "Upload aborted"
+  }
+  # upload
+  elseif($result -eq 1) {
+    $packages | % { 
+        $package = $_.Name
+        write-host "Uploading $package"
+#        NuGet push -source "http://packages.nuget.org/v1/" $package $key
+        write-host ""
+    }
+  }
+  popd
 }
+#}
 
 function Invoke-Packit
 {
@@ -38,9 +78,9 @@ function Invoke-Packit
 			 [Parameter(Position=1,Mandatory=0)]
     		 [string]$packageVersion = "",
 			 [Parameter(Position=2,Mandatory=0)]
-    		 [System.Collections.Hashtable]$dependencies,
+    		 [System.Collections.Hashtable]$dependencies = @{},
 			 [Parameter(Position=3, Mandatory=0)]
-			 [System.Collections.Hashtable]$assemplyNames = @{}
+			 [System.Collections.ArrayList]$assemblyNames = @{}
   		)
 		
 	begin
@@ -49,43 +89,124 @@ function Invoke-Packit
 	}
 	process
 	{
-		$version = $packageVersion;
+	
+    	[string]$version = $packageVersion
 		if($version -eq "")
 		{
 			try
 			{
-				 $version = GetVersionFromVersionAssembly				 
+				$versionAssemblyLocation = Resolve-Path -Path $script:packit.versionAssemblyName
+				[System.Reflection.Assembly]$versionAssembly = [System.Reflection.Assembly]::Loadfile($versionAssemblyLocation)
+				if($versionAssembly -ne $null)
+				{
+					$assmName = $versionAssembly.GetName();
+					if($assmName -ne $null){
+						$version = $assmName.version
+					}
+				}
 			}
 			catch
 			{
 			  "Unable to Find the Version from assembly due to the Error:- `n $_"
 		      $version = $script:packit.package_version
 			}
-			
-			$packageDir = "..\" + $packageName
-			del $packageDir
-			mkdir $packageDir
-			$packagePath = $packageDir + "\" + $packageName
-			NuGet spec $packagePath
-			<#
-			 Logic to Edit the .nuspec according to the parameter
-			 
-			#>
-			
-			<#
-			 Logic Copy the assemblies to lib
-			 Logic to Copy the Content
-			 Logic to copy the tools
-			 
-			#>
-			
 		}
+		$packageDir = "..\" + $packageName
+		if(Test-Path $packageDir)
+		{
+			Remove-Item $packageDir -Recurse
+		}
+		mkdir $packageDir 
+		$packagePath = $packageDir + "\" + $packageName
+		NuGet spec $packagePath
+		$nuGetSpecFile = $packagePath + ".nuspec"
+		[xml] $nuGetSpecContent= Get-Content $nuGetSpecFile
+		$nuGetSpecContent.package.metadata.Id = $packageName
+		$nuGetSpecContent.package.metadata.version = $version
+		$nuGetSpecContent.package.metadata.authors = $script:packit.package_authors
+		$nuGetSpecContent.package.metadata.owners = $script:packit.package_owners
+		$nuGetSpecContent.package.metadata.licenseUrl = $script:packit.package_licenseUrl
+		$nuGetSpecContent.package.metadata.projectUrl = $script:packit.package_projectUrl
+		$nuGetSpecContent.package.metadata.requireLicenseAcceptance = "true"
+		$nuGetSpecContent.package.metadata.description = $script:packit.package_description
+		$nuGetSpecContent.package.metadata.tags = $script:packit.package_tags
+		$nuGetSpecContent.package.metadata.iconUrl = $script:packit.package_iconUrl;
+#		if($nuGetSpecContent.package.metadata.language -eq $null)
+#		{
+#			$refNode = $nuGetSpecContent.package.metadata | Select-Object -Property "description"
+#			$parentNode = $nuGetSpecContent.package | Select-Object -Property "metadata"
+#			$languageNode = $parentNode.CreateElement("language");
+#			$parentNode.insertAfter($languageNode, $refNode)
+#			#$nuGetSpecContent.package.metadata.CreateElement("language");
+#			
+#		}
+#		$nuGetSpecContent.package.metadata.language = $script:packit.package_language
+		$dependencyInnerXml = ""
+		if($dependencies.Count -gt 0)
+		{
+		   $dependencies |  Foreach-Object {
+ 		   $p = $_
+    		@($p.GetEnumerator()) | Where-Object {            
+        	($_.Value | Out-String) 
+    		} | Foreach-Object {
+			 $dependencyPackage = $_.Key
+			 $dependencyPackageVersion = $_.Value
+			 if($dependencyPackageVersion -eq $VesionPlaceHolder)
+			 {
+			 	$dependencyPackageVersion = $version
+			 }
+			 $dependencyInnerXml = "{0}<dependency id=""{1}"" version=""{2}"" />" -f 
+        	 $dependencyInnerXml,$dependencyPackage,$dependencyPackageVersion
+    		}
+			}
+	       $nuGetSpecContent.package.metadata.dependencies.set_InnerXML($dependencyInnerXml)
+		}				 
+		Set-Content $nuGetSpecFile $nuGetSpecContent.get_OuterXML()				
+		 if($assemblyNames.Count -gt 0)
+		 {
+			 $libPath = $packageDir + "\lib"
+			 mkdir $libPath
+			 <#	 Logic Copy the assemblies to lib to support both 4.0 and 3.5 framework#>
+			 
+		 }
+		 $packageContentPath = ".\Content" + $packageName
+		 if(Test-Path $packageContentPath)
+		 {
+			 $contentPath = $packageDir + "\Content"
+			 mkdir $contentPath
+			 $packageContentPath += "\*.*"
+			 copy $packageContentPath $contentPath
+		 }
+		 $packageToolsPath = ".\Tools" + $packageName
+		 if(Test-Path $packageContentPath)
+		 {
+			 $toolsPath = $packageDir + "\tools"
+			 mkdir $toolsPath
+			 $packageToolsPath += "\*.*"
+			 copy $packageToolsPath $toolsPath
+		 }		 
+		 NuGet pack $nuGetSpecFile 
+		 
+		 if((Test-Path -Path $script:packit.packageOutPutDir) -ne $true)
+		 {
+		 	mkdir $script:packit.packageOutPutDir
+		 }
+		 $packName = "{0}.{1}.nupkg" -f
+		 $packageName, $version 
+		 
+#		 $relativePath = $script:packit.packageOutPutDir + "\" + $packageName
+#		 if(Test-Path $relativePath)
+#		 {
+#		 	Remove-Item $relativePath
+#		 }
+		 Move-Item $packName $script:packit.packageOutPutDir -Force
+		 PuchPackage($packName)
+		 
 	}
 	end
 	{
 	
-	}
-	
+	}	
 }
 
 Export-ModuleMember -Function "Invoke-Packit"
